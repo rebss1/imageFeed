6c2f1898ecdf7b25067aa57446a7cf6279318b85
loshilov ilya <loshilovilya@yandex.ru>
2024-01-10 18:30:26 +0300

    Initial Commit

---
+++ imageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  imageFeed
+//
+//  Created by Илья Лощилов on 10.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
