fd44bc9067ca7dec45b777caebbedc102ea89c16
Loshilov Ilya <loshilovilya@yandex.ru>
2024-05-07 17:12:05 +0300

    Исправлена ошибка после проверки интеграционным тестом

--- imageFeed/Presentation/ImageList/Services/ImagesListService.swift
+++ imageFeed/Presentation/ImageList/Services/ImagesListService.swift
@@ -21,35 +21,34 @@ final class ImagesListService {
     
     func fetchPhotosNextPage() {
         let nextPage = (lastLoadedPage ?? 0) + 1
-        if task != nil {
-            assert(Thread.isMainThread)
-            
-            let queryItems = [
-                URLQueryItem(name: ImagesListKeys.page.rawValue,
-                             value: "\(nextPage)"),
-                URLQueryItem(name: ImagesListKeys.perPage.rawValue,
-                             value: ImagesListConstants.photosPerPage)]
-            
-            guard let request = URLRequest.makeUrlRequest(
-                httpMethod: HTTPMethods.get.rawValue,
-                host: NetworkConstants.host,
-                path: ImagesListConstants.photosPath,
-                queryItems: queryItems) else { return }
-            
-            networkClient.fetch(urlRequest: request) { [weak self] (result: Result<[PhotoResult], Error>) in
-                switch result {
-                case .success(let photos):
-                    self?.lastLoadedPage = nextPage
-                    let lastPhotos = photos.map { Photo($0) }
-                    DispatchQueue.main.async {
-                        self?.photos.append(contentsOf: lastPhotos)
-                    }
-                    NotificationCenter.default.post(
-                        name: ImagesListService.didChangeNotification,
-                        object: self)
-                case .failure:
-                    break
+        
+        assert(Thread.isMainThread)
+        
+        let queryItems = [
+            URLQueryItem(name: ImagesListKeys.page.rawValue,
+                         value: "\(nextPage)"),
+            URLQueryItem(name: ImagesListKeys.perPage.rawValue,
+                         value: ImagesListConstants.photosPerPage)]
+        
+        guard let request = URLRequest.makeUrlRequest(
+            httpMethod: HTTPMethods.get.rawValue,
+            host: NetworkConstants.host,
+            path: ImagesListConstants.photosPath,
+            queryItems: queryItems) else { return }
+        
+        networkClient.fetch(urlRequest: request) { [weak self] (result: Result<[PhotoResult], Error>) in
+            switch result {
+            case .success(let photos):
+                self?.lastLoadedPage = nextPage
+                let lastPhotos = photos.map { Photo($0) }
+                DispatchQueue.main.async {
+                    self?.photos.append(contentsOf: lastPhotos)
                 }
+                NotificationCenter.default.post(
+                    name: ImagesListService.didChangeNotification,
+                    object: self)
+            case .failure:
+                break
             }
         }
     }
